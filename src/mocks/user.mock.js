a4a19c2aa3389482ad5ee9ace1e33a54dd3c69c6
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-08-01 20:58:24 -0300

    feat: adding mocked data

---
+++ src/mocks/user.mock.js
@@ -0,0 +1,20 @@
+export const user = {
+  profile: {
+    pic: 'pic uri',
+    name: 'Gabriel',
+    lastName: 'Moreira',
+    address: 'Argentina, Río Negro',
+    contact: {
+      phone: '+54 9 12345678',
+      email: 'gabriel.moreira1988@gmail.com',
+      other: [
+        {
+          label: '',
+          url: ''
+        }
+      ],
+      qr: 'uri qr'
+    },
+    about: 'this is a random description'
+  }
+}
--- src/templates/sections/Profile/Profile.js
+++ src/templates/sections/Profile/Profile.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Profile = () => {
+const Profile = ({ profile }) => {
   console.log('Profile')
-  return <></>
+  return (
+    <section>
+      Profile
+      <title>{profile.name}</title>
+    </section>
+  )
+}
+
+Profile.propTypes = {
+  profile: PropTypes.shape({
+    pic: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    contact: PropTypes.shape({
+      phone: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      other: PropTypes.arrayOf(
+        PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          url: PropTypes.string
+        })
+      ),
+      qr: PropTypes.string
+    }),
+    about: PropTypes.string
+  }).isRequired
 }
 
 export default Profile
