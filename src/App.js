79594bb92f90e6ee9ccf049d0b0c1ee4b67dc6ca
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-08-01 13:37:47 -0300

    update to arrow function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+)
 
 export default App
