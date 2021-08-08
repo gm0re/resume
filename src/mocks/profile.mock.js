3c4e79725952cfa47c6ec6e4868c727ab060e51d
Gabriel Moreira <gabriel.moreira1988@gmail.com>
2021-08-08 16:41:23 -0300

    feat: :sparkles: change profile structure and add more info

---
+++ src/mocks/profile.mock.js
@@ -0,0 +1,19 @@
+export const profile = {
+  pic: 'pic uri',
+  name: 'Gabriel',
+  lastName: 'Moreira',
+  profession: 'Engineer Manager',
+  address: 'Argentina, Río Negro',
+  contact: {
+    phone: '+54 9 12345678',
+    email: 'gabriel.moreira1988@gmail.com',
+    other: [
+      {
+        label: '',
+        url: ''
+      }
+    ],
+    qr: 'uri qr'
+  },
+  about: 'this is a random description'
+}
